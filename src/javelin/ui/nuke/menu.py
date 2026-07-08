5acc8378e1516f434eee242f9aedbf5e179dcd44
rfletchr <13312808+rfletchr@users.noreply.github.com>
2026-07-08 19:09:08 +0100

    WIP: defer attaching to panel till event loop starts

--- src/javelin/ui/nuke/menu.py
+++ src/javelin/ui/nuke/menu.py
@@ -1,9 +1,21 @@
 import nuke
 from nukescripts import panels
+from qtpy import QtCore
 
 from javelin.ui.panel.main import get_main_controller
 
 JAVELIN_MAIN_CONTROLLER = get_main_controller()
 JAVELIN_MAIN_CONTROLLER.populate()
 pane = nuke.getPaneFor("Properties.1")
-panels.registerWidgetAsPanel("JAVELIN_MAIN_CONTROLLER.get_view", "Javelin", "javelin.MainPanel", True).addToPane(pane)
+
+
+def attach():
+    panels.registerWidgetAsPanel(
+        "JAVELIN_MAIN_CONTROLLER.get_view",
+        "Javelin",
+        "javelin.MainPanel",
+        True,
+    ).addToPane(pane)
+
+
+QtCore.QTimer.singleShot(0, attach)
