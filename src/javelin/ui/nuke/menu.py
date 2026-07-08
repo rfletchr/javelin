c76f267d961af1625ebf0af6334c25a0213e984c
rfletchr <13312808+rfletchr@users.noreply.github.com>
2026-07-08 19:05:22 +0100

    WIP: Populate view on startup

--- src/javelin/ui/nuke/menu.py
+++ src/javelin/ui/nuke/menu.py
@@ -4,5 +4,6 @@ from nukescripts import panels
 from javelin.ui.panel.main import get_main_controller
 
 JAVELIN_MAIN_CONTROLLER = get_main_controller()
+JAVELIN_MAIN_CONTROLLER.populate()
 pane = nuke.getPaneFor("Properties.1")
 panels.registerWidgetAsPanel("JAVELIN_MAIN_CONTROLLER.get_view", "Javelin", "javelin.MainPanel", True).addToPane(pane)
